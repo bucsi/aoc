3f9c9add7e4951f69e0c51a730d86e84732838af
Bucsánszki Tamás <bucsitomi+git@gmail.com>
2019-12-03 14:38:40 +0100

    code cleanup, moved input into files

--- 1.py
+++ 1.py
@@ -1,19 +1,8 @@
-"""onle-liner for part 1
-with open("1.txt", "r") as infile:
-    print(sum([int(int(x)/3)-2 for x in infile.readlines()]))
-"""
-
-
 def calculate(mass):
     if mass < 6:
         return 0
     return int(mass/3-2) + calculate(int(mass/3-2))
 
 with open("1.txt", "r") as infile:
-    print(sum([calculate(int(x)) for x in infile.readlines()]))
-
-
-"""
-uzemanyaglist x = sum (map (uzemanyag) x) where 
-  uzemanyag x = x/3-2 + uzemanyag (x/3-2)
-"""
+    print("First part solution: ", sum([int(int(x)/3)-2 for x in infile.readlines()]))
+    print("Second part solution: ", sum([calculate(int(x)) for x in infile.readlines()]))
