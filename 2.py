de336a778e4bf12d56fb9349cd364b359d5a23be
Bucsánszki Tamás <bucsitomi+git@gmail.com>
2019-12-03 14:38:40 +0100

    code cleanup, moved input into files

--- 2.py
+++ 2.py
@@ -1,10 +1,30 @@
 noun = 0
 verb = 0
-output = 0
 cycle = 1
 
+def get_input(filename):
+    with open(filename, "r") as f:
+        return f.read().split(",")
+
+program = [int(n) for n in get_input("2.txt")]
+prog = program.copy()
+
+i = 0
+while(prog[i] != 99):
+    x = prog[prog[i+1]]
+    y = prog[prog[i+2]]
+    p = prog[i+3]
+    if(prog[i] == 1):
+        prog[p] = x+y
+    elif(prog[i] == 2):
+        prog[p] = x*y
+    else:
+        print(f"Hiba az {i}. mezoben: {prog[i]}")
+    i += 4
+print("First solution:", prog[0])
+
 while(1):
-    prog = [int(n) for n in "1,100,100,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,5,19,23,1,6,23,27,1,27,10,31,1,31,5,35,2,10,35,39,1,9,39,43,1,43,5,47,1,47,6,51,2,51,6,55,1,13,55,59,2,6,59,63,1,63,5,67,2,10,67,71,1,9,71,75,1,75,13,79,1,10,79,83,2,83,13,87,1,87,6,91,1,5,91,95,2,95,9,99,1,5,99,103,1,103,6,107,2,107,13,111,1,111,10,115,2,10,115,119,1,9,119,123,1,123,9,127,1,13,127,131,2,10,131,135,1,135,5,139,1,2,139,143,1,143,5,0,99,2,0,14,0".split(",")]
+    prog = program.copy()
     prog[1] = noun
     prog[2] = verb
     
@@ -13,16 +33,15 @@ while(1):
         x = prog[prog[i+1]]
         y = prog[prog[i+2]]
         p = prog[i+3]
-        #print(f"Utasitas-sorozat: {prog[i:i+4]}\t-> az {i+1}. szamot (erteke:{x}) és az {i+2}. szamot(erteke:{y}) fogom {p}-ba/be kiszamolni")
         if(prog[i] == 1):
             prog[p] = x+y
         elif(prog[i] == 2):
             prog[p] = x*y
         else:
-            print(f"Hiba az {i}. mezoben: {prog[i]}")
+            print(f"Error at mem {i}. Value: {prog[i]}")
         i += 4
-    print(f"{cycle}.ism:\t{noun}, {verb} -> {prog[0]}")
     if(prog[0] == 19690720):
+        print(f"At the {cycle}th repeat, i found: {noun} & {verb} -> {prog[0]} (second solution)")
         break
     else:
         cycle += 1
@@ -31,5 +50,5 @@ while(1):
             verb += 1
             noun = 0
             if verb == 100:
-                print("Tulmentem.")
+                print("No solution found for noun,verb 0..99")
                 break
