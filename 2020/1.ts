422c327c1b50f0f9af8e70019d9b77520687c7b2
bucsi <11545252+bucsi@users.noreply.github.com>
2020-12-02 17:25:14 +0100

    day2+formatting

--- 2020/1.ts
+++ 2020/1.ts
@@ -1,15 +1,14 @@
-const input = await Deno.readTextFile("./1.txt").then(r=>r.split("\n").map(el => parseInt(el)))
+const input = await Deno.readTextFile("./1.txt").then(r => r.split("\n").map(el => parseInt(el)))
 
-for(let i=0; i<input.length; i++){
-    for(let j=i+1; j<input.length; j++){
-        if(input[i] + input[j] === 2020){
+for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+        if (input[i] + input[j] === 2020) {
             console.log(`Two numbers solution: ${input[i] * input[j]}`)
         }
-        for(let k=j+1; k<input.length; k++){
-            if(input[i] + input[j] + input[k] === 2020){
+        for (let k = j + 1; k < input.length; k++) {
+            if (input[i] + input[j] + input[k] === 2020) {
                 console.log(`Three numbers solution: ${input[i] * input[j] * input[k]}`)
             }
         }
     }
 }
-
