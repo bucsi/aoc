991aa3ec5998b7d59d79d29b4f781dd8eb8c0299
bucsi <11545252+bucsi@users.noreply.github.com>
2020-12-02 17:25:14 +0100

    day2+formatting

---
+++ 2020/2.ts
@@ -0,0 +1,27 @@
+let converter = (arr: Array<string>): [number, number, string, string] => [
+    parseInt(arr[0]),
+    parseInt(arr[1]),
+    arr[2],
+    arr[4]
+]
+
+let text = Deno.readTextFile("./2.txt")
+
+text.then(r => r.split("\n"))
+    .then(a =>
+        a.reduce((total, curr) => {
+            let [min, max, char, passwd] = converter(curr.split(/-|\s|:/))
+            let count = (passwd.match(new RegExp(char, "g")) || []).length
+            return total + (min <= count && count <= max ? 1 : 0)
+        }, 0)
+    )
+    .then(res => console.log(`Valid passwords for task 1: ${res}`))
+
+text.then(r => r.split("\n"))
+    .then(a =>
+        a.reduce((total, curr) => {
+            let [pos1, pos2, char, passwd] = converter(curr.split(/-|\s|:/))
+            return total + ((passwd[pos1 - 1] === char) !== (passwd[pos2 - 1] === char) ? 1 : 0)
+        }, 0)
+    )
+    .then(res => console.log(`Valid passwords for task 2: ${res}`))
