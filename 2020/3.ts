fb45b783881b4ed40da1a63c050de45dd32893ac
bucsi <11545252+bucsi@users.noreply.github.com>
2020-12-05 14:36:40 +0100

    day3 complete

--- 2020/3.ts
+++ 2020/3.ts
@@ -1,28 +1,43 @@
 export {}
 const input = await Deno.readTextFile("./3.txt").then(r => r.split("\n"))
 
+
+const len = input[0].length-2;
+console.log(`Rowlength is: ${len}`)
+
+
+const positions = [0,0,0,0,0]
+const trees = [0,0,0,0,0]
+
+/*
+0 right 1 down 1
+1 right 3 down 1
+2 right 3 down 1
+3 right 3 down 1
+4 right 1 down 2
+
+*/
+
 for(let i=0; i<input.length; i++){
-    input[i] = input[i].slice(0,-1)
-}
-
-const len = input[0].length-1;
-for(let c of input[0]){
-    console.log(c)
-}
-console.log(`${len} hosszú egy sor`)
-let pos = 0
-let trees = 0
-
-for(const line of input){
-    console.log(`${line} (${pos})`)
-    console.log(`${" ".repeat(pos)}^`)
-    if(line[pos] === "#") trees++
-    pos += 3
-    if(pos > len){
-        const offset = pos-len-1
-        pos = 0
-        pos += offset
+    let line = input[i].slice(0,-1)
+    for(let p=0; p<4; p++){
+        if(line[positions[p]] === "#") trees[p]++
+    }
+    if(i%2===0){
+        if(line[positions[4]] === "#") trees[4]++
+        positions[4]+=1
+    }
+    positions[0] += 1
+    positions[1] += 3
+    positions[2] += 5
+    positions[3] += 7
+    for(let p=0; p<=4; p++){
+        if(positions[p] > len){
+            positions[p] = 0 + positions[p]-len-1
+        }
     }
 }
 
-console.log(trees)
+console.log(`Trees found when going right 3 down 1: ${trees[1]}`)
+console.log(`Trees found in all directions: ${trees}`)
+console.log(`Task 2 solution is: ${trees.reduce((p,e)=>p*=e)}`)
